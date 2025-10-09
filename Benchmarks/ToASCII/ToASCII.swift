4415c7a6917de5a34e675bfc135700eead368a6c
MahdiBM <github@mahdibm.com>
2025-10-09 12:39:32 +0330

    Rename benchmarks to deduplicate names

--- Benchmarks/ToASCII/ToASCII.swift
+++ Benchmarks/ToASCII/ToASCII.swift
@@ -19,7 +19,7 @@ let benchmarks: @Sendable () -> Void = {
         /// Mark: - google.com
 
         Benchmark(
-            "\(namePrefix)_google_dot_com_CPU_500K",
+            "To_ASCII_\(namePrefix)_google_dot_com_CPU_500K",
             configuration: .init(
                 metrics: [.cpuUser],
                 warmupIterations: 5,
@@ -34,7 +34,7 @@ let benchmarks: @Sendable () -> Void = {
         }
 
         Benchmark(
-            "\(namePrefix)_google_dot_com_Malloc",
+            "To_ASCII_\(namePrefix)_google_dot_com_Malloc",
             configuration: .init(
                 metrics: [.mallocCountTotal],
                 warmupIterations: 1,
@@ -49,7 +49,7 @@ let benchmarks: @Sendable () -> Void = {
         /// Mark: - app-analytics-services.com
 
         Benchmark(
-            "\(namePrefix)_app-analytics-services_dot_com_CPU_500K",
+            "To_ASCII_\(namePrefix)_app-analytics-services_dot_com_CPU_500K",
             configuration: .init(
                 metrics: [.cpuUser],
                 warmupIterations: 5,
@@ -64,7 +64,7 @@ let benchmarks: @Sendable () -> Void = {
         }
 
         Benchmark(
-            "\(namePrefix)_app-analytics-services_dot_com_Malloc",
+            "To_ASCII_\(namePrefix)_app-analytics-services_dot_com_Malloc",
             configuration: .init(
                 metrics: [.mallocCountTotal],
                 warmupIterations: 1,
@@ -80,7 +80,7 @@ let benchmarks: @Sendable () -> Void = {
         /// Grabbed from Cloudflare top 1M domains
 
         Benchmark(
-            "\(namePrefix)_öob_dot_se_CPU_200K",
+            "To_ASCII_\(namePrefix)_öob_dot_se_CPU_200K",
             configuration: .init(
                 metrics: [.cpuUser],
                 warmupIterations: 5,
@@ -95,7 +95,7 @@ let benchmarks: @Sendable () -> Void = {
         }
 
         Benchmark(
-            "\(namePrefix)_öob_dot_se_Malloc",
+            "To_ASCII_\(namePrefix)_öob_dot_se_Malloc",
             configuration: .init(
                 metrics: [.mallocCountTotal],
                 warmupIterations: 1,
@@ -111,7 +111,7 @@ let benchmarks: @Sendable () -> Void = {
         /// Grabbed from Cloudflare top 100K domains
 
         Benchmark(
-            "\(namePrefix)_生命之花_dot_中国_CPU_200K",
+            "To_ASCII_\(namePrefix)_生命之花_dot_中国_CPU_200K",
             configuration: .init(
                 metrics: [.cpuUser],
                 warmupIterations: 5,
@@ -126,7 +126,7 @@ let benchmarks: @Sendable () -> Void = {
         }
 
         Benchmark(
-            "\(namePrefix)_生命之花_dot_中国_Malloc",
+            "To_ASCII_\(namePrefix)_生命之花_dot_中国_Malloc",
             configuration: .init(
                 metrics: [.mallocCountTotal],
                 warmupIterations: 1,
