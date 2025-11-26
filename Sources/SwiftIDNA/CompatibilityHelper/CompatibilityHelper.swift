12e7a29bb0f9f872769179fcf2312398e50e4e08
MahdiBM <github@mahdibm.com>
2025-11-26 15:29:32 +0330

    more @inlinable

--- Sources/SwiftIDNA/CompatibilityHelper/CompatibilityHelper.swift
+++ Sources/SwiftIDNA/CompatibilityHelper/CompatibilityHelper.swift
@@ -1,6 +1,7 @@
 @available(swiftIDNAApplePlatforms 10.15, *)
 @usableFromInline
 protocol CompatibilityHelper: Sendable, ~Copyable {
+    @inlinable
     func makeString(
         unsafeUninitializedCapacity capacity: Int,
         initializingWith initializer: (
@@ -8,32 +9,39 @@ protocol CompatibilityHelper: Sendable, ~Copyable {
         ) throws -> Void
     ) rethrows -> String
 
+    @inlinable
     func makeString(
         _uncheckedAssumingValidUTF8 span: Span<UInt8>
     ) -> String
 
+    @inlinable
     func withSpan<T>(
         for bytes: [UInt8],
         _ body: (Span<UInt8>) throws -> T
     ) rethrows -> T
 
+    @inlinable
     func withSpan<T, E: Error>(
         for string: inout String,
         _ body: (Span<UInt8>) throws(E) -> T
     ) throws(E) -> T
 
+    @inlinable
     func withSpan<T, E: Error>(
         for substring: inout Substring,
         _ body: (Span<UInt8>) throws(E) -> T
     ) throws(E) -> T
 
+    @inlinable
     func isInNFC(span: Span<UInt8>) -> Bool
 
+    @inlinable
     func _uncheckedAssumingValidUTF8_ensureNFC(
         on bytes: inout [UInt8]
     )
 
     @_lifetime(copy span)
+    @inlinable
     func makeUnicodeScalarIterator(
         of span: Span<UInt8>
     ) -> any UnicodeScalarsIteratorProtocol & ~Escapable
